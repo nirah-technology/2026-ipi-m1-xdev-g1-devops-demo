16af622ef92580ed5440a7a44dada0c64ad10f01
Nicolas Metivier <nicolas.a.metivier@gmail.com>
2026-04-22 13:06:04 +0200

    Add Posts rest controller

--- src/main/java/fr/nirahtech/devopsdemo/controllers/PostsRestController.java
+++ src/main/java/fr/nirahtech/devopsdemo/controllers/PostsRestController.java
@@ -1,11 +1,14 @@
 package fr.nirahtech.devopsdemo.controllers;
 
+import java.util.Objects;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.nirahtech.devopsdemo.domain.Post;
@@ -22,8 +25,14 @@ public class PostsRestController {
     }
 
     @GetMapping("")
-    public Page<Post> findAllPosts(Pageable pageable) {
-        return this.postsRepository.findAll(pageable);
+    public Page<Post> findAllPosts(Pageable pageable, @RequestParam(name="user_id", required = false) Integer userId) {
+        final Page<Post> posts;
+        if (Objects.isNull(userId)) {
+            posts = this.postsRepository.findAll(pageable);
+        } else {
+            posts = this.postsRepository.findAllByUserId(userId, pageable);
+        }
+        return posts;
     }
 
     @PostMapping("")
--- src/main/java/fr/nirahtech/devopsdemo/repositories/PostsRepository.java
+++ src/main/java/fr/nirahtech/devopsdemo/repositories/PostsRepository.java
@@ -1,7 +1,7 @@
 package fr.nirahtech.devopsdemo.repositories;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +9,5 @@ import fr.nirahtech.devopsdemo.domain.Post;
 
 @Repository
 public interface PostsRepository extends JpaRepository<Post, Integer> {
-    List<Post> findAllByUserId(Integer userId);
+    Page<Post> findAllByUserId(Integer userId, Pageable pageable);
 }
